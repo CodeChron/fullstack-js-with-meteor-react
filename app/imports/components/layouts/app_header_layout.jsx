ed3826b5fefdab1102d0048683c14470ca2e4c99
Anders Ramsay <andersr@gmail.com>
2016-04-30 14:10:02 -0400

    custom header layout

--- app/imports/components/layouts/app_header_layout.jsx
+++ app/imports/components/layouts/app_header_layout.jsx
@@ -3,11 +3,8 @@ import { IconBtn } from '../buttons/icon_btn'
 
 export const AppHeaderLayout = (props) =>
 <nav id="app-header" className="container three-col-layout">
-  <IconBtn isMaterialIcon={true} icon="clear" title="Cancel" />
   <h1 className="page-title main-content">{props.pageTitle}</h1>
-   <IconBtn isMaterialIcon={true} icon="more_vert"  title="More options"/>
 </nav>
-
 
 AppHeaderLayout.propTypes = {
   pageTitle: React.PropTypes.string
