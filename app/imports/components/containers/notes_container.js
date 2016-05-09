af0b0532feb2a1b4665625468c7a6f3a7994c554
Anders Ramsay <andersr@gmail.com>
2016-05-09 14:22:09 -0400

    redirect on create

--- app/imports/components/containers/notes_container.js
+++ app/imports/components/containers/notes_container.js
@@ -2,30 +2,34 @@ import { createContainer } from 'meteor/react-meteor-data'
 import { Notes } from '../../api/notes/notes'
 import { List } from '../lists/list'
 import { Meteor } from 'meteor/meteor'
+import { FlowRouter } from 'meteor/kadira:flow-router'
 
 export default createContainer(() => {
 
-	const sub = Meteor.subscribe('notes.all')
+	const
+    sub = Meteor.subscribe('notes.all'),
 
-	const notes = sub.ready()? Notes.find({}, { sort: { updatedAt: -1 }}).fetch() : []
+    notes = sub.ready()? Notes.find({}, { sort: { updatedAt: -1 }}).fetch() : [],
 
-	const handleCreate = (title) => {
-    Meteor.call('/note/create', title, (err, result) => {
-      if (!err) {
-        // console.log('note: ' + result._id)
-      } else {
-        console.log('there was an error: ' + err.reason)
-      }
-    })
-	}
+    redirectToNoteDetail = note => FlowRouter.go("noteDetail", {_id: note._id}),
 
-	const handleDelete = (note) => {
-		Meteor.call('/note/delete', note._id, (err, result) => {
-      if (err) {
-        console.log('there was an error: ' + err.reason)
-      }
-    })
-	}
+    handleCreate = (title) => {
+      Meteor.call('/note/create', title, (err, result) => {
+        if (!err) {
+          redirectToNoteDetail(result)
+        } else {
+          console.log('there was an error: ' + err.reason)
+        }
+      })
+  	},
+    
+    handleDelete = (note) => {
+  		Meteor.call('/note/delete', note._id, (err, result) => {
+        if (err) {
+          console.log('there was an error: ' + err.reason)
+        }
+      })
+  	}
 
   return {
   	handleSubmit: handleCreate,
