58c18c574ed7ecdf6db0a2e69ebf16b586981a39
Anders Ramsay <andersr@gmail.com>
2016-05-27 11:45:54 -0400

    use AppLayout for NotesContainer

---
+++ app/imports/components/notes/notes_container.js
@@ -0,0 +1,21 @@
+import { createContainer } from 'meteor/react-meteor-data'
+import { Notes } from '../../api/notes/notes'
+import { AppLayout } from '../layouts/app_layout'
+
+// import { SingleFieldSubmit } from '../forms/single_field_submit'
+
+export default createContainer(() => {
+	
+	const handleCreateNote = (content) => {
+		Notes.insert({ 
+			content:content,
+			updatedAt: new Date() 
+		})
+	}
+
+  return {
+  	handleSubmit: handleCreateNote,
+	  placeholder: "New Note",
+  }
+
+}, AppLayout)
