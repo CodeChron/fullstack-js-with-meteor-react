d56d3580cd53438f9296d51e672ec96f44e0568e
Anders Ramsay <andersr@gmail.com>
2016-05-27 12:25:50 -0400

    added singleFieldSubmit to AppLayout

--- app/imports/components/layouts/app_layout.jsx
+++ app/imports/components/layouts/app_layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { AppHeaderLayout } from './app_header_layout'
+import { SingleFieldSubmit } from '../forms/single_field_submit'
 
 export const AppLayout = () =>
   <div id="app-container">
     <AppHeaderLayout />
     <div id="main-content" className="container">
-     Main Content
+     <SingleFieldSubmit />
     </div>
   </div>
