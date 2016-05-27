a908f5d9944f48de51772511371490ed52c624a8
Anders Ramsay <andersr@gmail.com>
2016-05-27 13:35:10 -0400

    make NotesContainer top-level component

--- app/imports/components/containers/notes_container.js
+++ app/imports/components/containers/notes_container.js
@@ -1,7 +1,6 @@
 import { createContainer } from 'meteor/react-meteor-data'
 import { Notes } from '../../api/notes/notes'
 import { AppLayout } from '../layouts/app_layout'
-// import { SingleFieldSubmit } from '../forms/single_field_submit'
 
 export default createContainer(
 	() => {
--- app/imports/startup/client/main.js
+++ app/imports/startup/client/main.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import NotesContainer from '/imports/components/containers/notes_container'
-// import { AppLayout } from '/imports/components/layouts/app_layout'
 
 Meteor.startup(() =>
 	ReactDOM.render(<NotesContainer />, document.getElementById("app"))
