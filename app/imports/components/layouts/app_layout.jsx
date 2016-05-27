74397c9089f1184d4ce34581be41464e5d0ab975
Anders Ramsay <andersr@gmail.com>
2016-05-27 14:02:21 -0400

    cleanup

--- app/imports/components/layouts/app_layout.jsx
+++ app/imports/components/layouts/app_layout.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { AppHeaderLayout } from './app_header_layout'
 import { List } from '../lists/list'
-// import { SingleFieldSubmit } from '../forms/single_field_submit'
-// <SingleFieldSubmit handleSubmit={props.handleSubmit} placeholder={props.placeholder}  />
 
 export const AppLayout = (props) =>
   <div id="app-container">
